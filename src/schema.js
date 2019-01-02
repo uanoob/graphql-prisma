7da788579cdec0841a28fa5e475aa441abb59879
uanoob <usnet@ukr.net>
2019-01-02 13:05:02 +0200

    Create graphql schema: User, Post, Comment & Query

---
+++ src/schema.js
@@ -0,0 +1,33 @@
+const typeDefs = `
+  type Query {
+    users(query: String): [User!]!
+    posts(query: String): [Post!]!
+    comments: [Comment!]!
+    me: User!
+    post: Post!
+  },
+  type User {
+    id: ID!
+    name: String!
+    email: String!
+    age: Int
+    posts: [Post!]!
+    comments: [Comment!]
+  },
+  type Post {
+    id: ID!
+    title: String!
+    body: String!
+    published: Boolean!
+    author: User!
+    comments: [Comment!]
+  }
+  type Comment {
+    id: ID!
+    text: String!
+    author: User!
+    post: Post!
+  }
+`;
+
+export { typeDefs as default };
