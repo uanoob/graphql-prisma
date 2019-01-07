763e99df0b4bbca0cd25a98f34fcec17ec51761b
uanoob <usnet@ukr.net>
2019-01-07 09:55:53 +0200

    Add functions: createPostForUser & updatePostForUser

--- src/prisma.js
+++ src/prisma.js
@@ -5,14 +5,57 @@ const prisma = new Prisma({
   endpoint: 'http://localhost:4466',
 });
 
-prisma.query
-  .users(null, '{ id name email posts { id title } }')
-  .then((data) => {
-    console.log(JSON.stringify(data, undefined, 2));
-  })
-  .catch(err => console.log(err));
+const createPostForUser = async (authorId, data) => {
+  const userExist = await prisma.exists.User({
+    id: authorId,
+  });
+  if (!userExist) {
+    throw new Error('User not found.');
+  }
+  const post = await prisma.mutation.createPost(
+    {
+      data: {
+        ...data,
+        author: {
+          connect: {
+            id: authorId,
+          },
+        },
+      },
+    },
+    '{ author { id name email posts { id title body published } } }',
+  );
+  return post.author;
+};
 
-prisma.query
-  .comments(null, '{ id text author { id name} post { title }}')
-  .then(data => console.log(JSON.stringify(data, undefined, 2)))
-  .catch(err => console.log(err));
+// createPostForUser('cjqkqth0q001b0945zg4i4m1y', {
+//   title: 'React and Redux',
+//   body: 'Redux for everybody',
+//   published: false,
+// })
+//   .then(user => console.log(JSON.stringify(user, undefined, 2)))
+//   .catch(err => console.log(err.message));
+
+const updatePostForUser = async (postId, data) => {
+  const postExist = await prisma.exists.Post({
+    id: postId,
+  });
+  if (!postExist) {
+    throw new Error('Post not found');
+  }
+  const post = await prisma.mutation.updatePost(
+    {
+      data,
+      where: { id: postId },
+    },
+    '{ author { id name email posts { id title body published} } }',
+  );
+  return post.author;
+};
+
+// updatePostForUser('cjqkuaq21002i09453jwovke7', {
+//   title: 'Angie and Friends',
+//   published: false,
+// })
+//   .then(user => console.log(JSON.stringify(user, undefined, 2)))
+//   .catch(err => console.log(err.message));
