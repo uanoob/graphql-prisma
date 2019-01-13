bd3b1160a0b8859b4456bac54befc23ec3d11a6f
uanoob <usnet@ukr.net>
2019-01-13 11:30:49 +0200

    Create operations -> use graphql variables

---
+++ tests/utils/operations.js
@@ -0,0 +1,114 @@
+import { gql } from 'apollo-boost';
+
+const createUser = gql`
+  mutation($data: CreateUserInput!) {
+    createUser(data: $data) {
+      user {
+        id
+        name
+        email
+      }
+      token
+    }
+  }
+`;
+
+const getUsers = gql`
+  query {
+    users {
+      id
+      name
+      email
+    }
+  }
+`;
+
+const login = gql`
+  mutation($data: LoginUserInput!) {
+    login(data: $data) {
+      token
+      user {
+        id
+        name
+        email
+      }
+    }
+  }
+`;
+
+const getProfile = gql`
+  query {
+    me {
+      id
+      name
+      email
+    }
+  }
+`;
+
+const getPosts = gql`
+  query {
+    posts {
+      id
+      title
+      body
+      published
+    }
+  }
+`;
+
+const getMyPosts = gql`
+  query {
+    myPosts {
+      id
+      title
+      body
+      published
+    }
+  }
+`;
+
+const createPost = gql`
+  mutation($data: CreatePostInput!) {
+    createPost(data: $data) {
+      id
+      title
+      body
+      published
+    }
+  }
+`;
+
+const updatePost = gql`
+  mutation($id: ID!, $data: UpdatePostInput!) {
+    updatePost(id: $id, data: $data) {
+      id
+      title
+      body
+      published
+    }
+  }
+`;
+
+const deletePost = gql`
+  mutation($id: ID!) {
+    deletePost(id: $id) {
+      id
+      title
+      body
+      published
+    }
+  }
+`;
+
+export {
+  createUser,
+  getProfile,
+  getUsers,
+  login,
+  getPosts,
+  getMyPosts,
+  createPost,
+  updatePost,
+  deletePost,
+};
