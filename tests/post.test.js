c4b9852c44d5088f195e0cd5254162cce21bacc9
uanoob <usnet@ukr.net>
2019-01-13 11:28:39 +0200

    Create tests with auth -> user & post

--- tests/post.test.js
+++ tests/post.test.js
@@ -1,24 +1,74 @@
 import 'cross-fetch/polyfill';
-import { gql } from 'apollo-boost';
-import client from './utils/getClient';
-import seedDatabase from './utils/seedDatabase';
+import {
+  getPosts, getMyPosts, createPost, updatePost, deletePost,
+} from './utils/operations';
+import getClient from './utils/getClient';
+import seedDatabase, { userOne, postOne, postTwo } from './utils/seedDatabase';
+import prisma from '../src/prisma';
+
+const client = getClient();
 
 beforeEach(seedDatabase);
 
 test('Should expose published posts', async () => {
-  const getPosts = gql`
-    query {
-      posts {
-        id
-        title
-        body
-        published
-      }
-    }
-  `;
   const response = await client.query({
     query: getPosts,
   });
   expect(response.data.posts.length).toBe(1);
   expect(response.data.posts[0].published).toBe(true);
+});
+test('Should fetch user posts', async () => {
+  const clientWithAuth = getClient(userOne.jwt);
+
+  const { data } = await clientWithAuth.query({
+    query: getMyPosts,
+  });
+  expect(data.myPosts.length).toBe(2);
+});
+test('Should be able to update own post', async () => {
+  const clientWithAuth = getClient(userOne.jwt);
+  const variables = {
+    id: postOne.post.id,
+    data: {
+      published: false,
+    },
+  };
+  const { data } = await clientWithAuth.mutate({ mutation: updatePost, variables });
+  const postExists = await prisma.exists.Post({
+    id: postOne.post.id,
+    published: false,
+  });
+  expect(data.updatePost.published).toBe(false);
+  expect(postExists).toBe(true);
+});
+test('Should be able to create new post', async () => {
+  const clientWithAuth = getClient(userOne.jwt);
+  const variables = {
+    data: {
+      title: 'Post title',
+      body: '...',
+      published: true,
+    },
+  };
+  const { data } = await clientWithAuth.mutate({
+    mutation: createPost,
+    variables,
+  });
+  expect(data.createPost.title).toBe('Post title');
+  expect(data.createPost.body).toBe('...');
+  expect(data.createPost.published).toBe(true);
+});
+test('Should be able delete post', async () => {
+  const clientWithAuth = getClient(userOne.jwt);
+  const variables = {
+    id: postTwo.post.id,
+  };
+  await clientWithAuth.mutate({
+    mutation: deletePost,
+    variables,
+  });
+  const existPost = await prisma.exists.Post({
+    id: postTwo.post.id,
+  });
+  expect(existPost).toBe(false);
 });
--- tests/user.test.js
+++ tests/user.test.js
@@ -1,40 +1,31 @@
 import 'cross-fetch/polyfill';
-import { gql } from 'apollo-boost';
-import client from './utils/getClient';
-import seedDatabase from './utils/seedDatabase';
+import {
+  createUser, getProfile, getUsers, login,
+} from './utils/operations';
+import getClient from './utils/getClient';
+import seedDatabase, { userOne } from './utils/seedDatabase';
 import prisma from '../src/prisma';
+
+const client = getClient();
 
 beforeEach(seedDatabase);
 
 test('Should create new user', async () => {
-  const createUser = gql`
-    mutation {
-      createUser(data: { name: "blue", email: "blue@icloud.com", password: "blue@icloud.com" }) {
-        user {
-          id
-          name
-          email
-        }
-        token
-      }
-    }
-  `;
+  const variables = {
+    data: {
+      name: 'blue',
+      email: 'blue@icloud.com',
+      password: 'blue@icloud.com',
+    },
+  };
   const response = await client.mutate({
     mutation: createUser,
+    variables,
   });
   const exists = await prisma.exists.User({ id: response.data.createUser.user.id });
   expect(exists).toBe(true);
 });
 test('Should expose public author profile', async () => {
-  const getUsers = gql`
-    query {
-      users {
-        id
-        name
-        email
-      }
-    }
-  `;
   const response = await client.query({
     query: getUsers,
   });
@@ -43,36 +34,36 @@ test('Should expose public author profile', async () => {
   expect(response.data.users[0].name).toBe('red');
 });
 test('Should not login with bad credintials', async () => {
-  const login = gql`
-    mutation {
-      login(data: { email: "red@icloud.com", password: "dju4i2Rt3ui" }) {
-        token
-        user {
-          id
-          name
-          email
-        }
-      }
-    }
-  `;
-  await expect(client.mutate({ mutation: login })).rejects.toThrow();
+  const variables = {
+    data: {
+      email: 'red@icloud.com',
+      password: 'dju4i2Rt3ui',
+    },
+  };
+  await expect(client.mutate({ mutation: login, variables })).rejects.toThrow();
 });
 test('Should not signup user with short password', async () => {
-  const createUser = gql`
-    mutation {
-      createUser(data: { name: "yellow", email: "yellow@icloud.com", password: "yellow" }) {
-        token
-        user {
-          id
-          name
-          email
-        }
-      }
-    }
-  `;
+  const variables = {
+    data: {
+      name: 'yellow',
+      email: 'yellow@icloud.com',
+      password: 'yellow',
+    },
+  };
   await expect(
     client.mutate({
       mutation: createUser,
+      variables,
     }),
   ).rejects.toThrow();
+});
+test('Should fetch user profile', async () => {
+  const clientWithAuth = getClient(userOne.jwt);
+
+  const { data } = await clientWithAuth.query({
+    query: getProfile,
+  });
+  expect(data.me.id).toBe(userOne.user.id);
+  expect(data.me.name).toBe(userOne.user.name);
+  expect(data.me.email).toBe(userOne.user.email);
 });
